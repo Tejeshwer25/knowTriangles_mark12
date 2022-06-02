85bc5e9456e56325a23aefe8cf160f9b14cd3383
Tejeshwer Singh Sachdeva <tejeshwersingh2504@gmail.com>
2022-06-02 20:02:59 +0530

    Find Area page completed
    - Find Area page completed with the respective functionalities to calculate the area of a triangle

---
+++ js/findArea.js
@@ -0,0 +1,30 @@
+const firstSide = document.querySelector("#firstSide");
+const secondSide = document.querySelector("#secondSide");
+const thirdSide = document.querySelector("#thirdSide");
+const submitBtn = document.querySelector("#submitBtn");
+const result = document.querySelector("#result");
+
+submitBtn.addEventListener("click", findArea);
+
+function findArea(e) {
+  e.preventDefault();
+
+  const a = parseFloat(firstSide.value);
+  const b = parseFloat(secondSide.value);
+  const c = parseFloat(thirdSide.value);
+
+  if (!a || !b || !c) {
+    result.innerHTML = "Please enter a valid value";
+    return;
+  }
+
+  const semiPerimeter = (a + b + c) / 2;
+  const area = Math.sqrt(
+    semiPerimeter *
+      (semiPerimeter - a) *
+      (semiPerimeter - b) *
+      (semiPerimeter - c)
+  );
+
+  result.textContent = `The area of the triangle is ${area.toFixed(3)} units`;
+}
